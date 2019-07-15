fbeefd9116f698cb1e3d476aa1cb69c522ab409f
Belendia Abdissa <belendia@gmail.com>
2019-07-15 22:08:46 +0300

    Connect the views to urls and setup registeration for group model

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+# this is to edit the GroupMember model inline in the Group app interface in the admin app
+
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
